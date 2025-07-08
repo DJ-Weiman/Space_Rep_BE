9614e6f59578f11dd3f098a7a1ac43e5233cc2fc
Dion Weiman <dion@4axissolutions.com>
2025-07-08 12:58:02 +0530

    Initial Commit

---
+++ userservice/src/main/java/com/djw/userservice/model/UserEntity.java
@@ -0,0 +1,36 @@
+package com.djw.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String keycloakId;
+
+    @Column(nullable = false)
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
